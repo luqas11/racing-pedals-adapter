8f5b6f2844262c0006fe041e7a71a508e9006088
luqas11 <lucascazas@hotmail.com>
2024-12-24 20:37:40 -0300

    Format firmware sketch with the Arduino IDE 2 auto formatter

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,21 +5,16 @@ Joystick_ Joystick;
 // If true, disables the joystick and sends the input data to the serial monitor instead
 const bool DEBUG_MODE = false;
 
-void setup()
-{
+void setup() {
   // Initialize objects
-  if (DEBUG_MODE)
-  {
+  if (DEBUG_MODE) {
     Serial.begin(9600);
-  }
-  else
-  {
+  } else {
     Joystick.begin();
   }
 }
 
-void loop()
-{
+void loop() {
   // Read the analog inputs and map the raw values to acceptable values for the Joystick object
   int accelerator = analogRead(A0);
   accelerator = log(accelerator - 45) * 50;
@@ -31,17 +26,14 @@ void loop()
   brake = constrain(brake, 120, 330);
   brake = map(brake, 120, 330, 0, 1023);
 
-  if (DEBUG_MODE)
-  {
+  if (DEBUG_MODE) {
     // Print the raw input values in the serial monitor
     Serial.println();
     Serial.println("Raw analog inputs values:");
     Serial.println("Accelerator: " + (String)(analogRead(A0)));
     Serial.println("Brake: " + (String)(analogRead(A2)));
     delay(200);
-  }
-  else
-  {
+  } else {
     // Set the mapped input values to the Joystick axes
     Joystick.setRxAxis(accelerator);
     Joystick.setRyAxis(brake);
